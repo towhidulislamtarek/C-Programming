51705cc5ae60b33bc166baa3ede7a421c5fb6157
Towhidul Islam Tarek <tarektowhidulislam@gmail.com>
2025-11-22 12:47:36 +0600

    Add files via upload

---
+++ Switch/basic.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    char ch;
+
+    cout << "Enter this letter: ";
+    cin >> ch;
+
+    switch (ch)
+    {
+    case 'a':
+        cout << "Vowel";
+        break;
+
+    case 'e':
+        cout << "Vowel";
+        break;
+
+    case 'i':
+        cout << "Vowel";
+        break;
+    case 'o':
+        cout << "Vowel";
+        break;
+    case 'u':
+        cout << "Vowel";
+        break;
+    }
+}
